83be7be577e3087a8ab37d82bb6e2fc8b6301b83
vzmanjunathreddy <vz.manjunathreddy@gmail.com>
2023-01-30 18:30:46 +0530

    Add project files.

---
+++ ConsoleFrameWorkBasics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFrameWorkBasics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("I am from .Net Framework");
+            
+            Console.WriteLine("Welcome to manual execution");
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ ControlStatements/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatements
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            #region  Datatypes in C#
+            int firstNumber =10;
+            float price=120;
+            double quantity=20.8;
+            DateTime todaysDate= DateTime.Now;
+            bool hasValue = false;
+    
+            if(args.Length> 0)
+            {
+                hasValue= true;
+                Console.WriteLine($"Boolean value is set to true and value is {args[0]}");
+            }
+
+            Console.WriteLine("FirstNumber is {0}",+firstNumber);
+
+            Console.WriteLine($"FirstNumber is {firstNumber}");
+
+            Console.WriteLine($"price is {price}");
+
+            Console.WriteLine($"quantity is {quantity}");
+            Console.WriteLine($"Todays date is {todaysDate}");
+           
+
+            Console.WriteLine("$Enter the Second Number");
+
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            var thirdNumber = 20;
+
+            Console.WriteLine($"SecondNumber Entered is {secondNumber}");
+
+
+            Console.WriteLine($"Sum of two numbers is {firstNumber + secondNumber} ");
+
+
+            Console.ReadKey();
+
+            //Conditions, Switch , While
+
+            if(firstNumber>secondNumber)
+            {
+                Console.WriteLine($"Firstnumber is greater than second number");
+            }
+            if(firstNumber==secondNumber)
+            {
+                Console.WriteLine($"Numbers are equest");
+
+            }
+            if(!hasValue)
+            {
+                Console.WriteLine("No Args passed in commandline");
+            }
+            else
+            {
+                Console.WriteLine($"Second is greater than First number");
+            }
+
+
+        }
+        #endregion         
+    }
+}
